fef4a0c1b12dc0de4b65f356d7f0e7cada2360eb
Alejandro Martinez <alexito4@gmail.com>
2013-08-07 18:17:56 +0200

    Implement NSCoding in BBJoinResult

--- Backbeam/BBJoinResult.m
+++ Backbeam/BBJoinResult.m
@@ -10,6 +10,20 @@
 
 @implementation BBJoinResult
 
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super init];
+    if (self) {
+        _count = [aDecoder decodeIntegerForKey:@"count"];
+        _objects = [aDecoder decodeObjectForKey:@"objects"];
+    }
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+    [aCoder encodeInteger:self.count forKey:@"count"];
+    [aCoder encodeObject:self.objects forKey:@"objects"];
+}
+
 - (NSString *)description {
     return [NSString stringWithFormat:@"count=%d, results=%@",
             self.count,
